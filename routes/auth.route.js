a5a343d4149c0c55277e95918bd4be8c10b34bc9
Jhon Arlem Andrade <jhondoncan@outlook.com>
2023-02-15 17:33:26 -0500

    feat: :sparkles: Se crea rutas POST para /login y /registro

---
+++ routes/auth.route.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const router = express.Router();
+
+router.post('/login', (req, res) => {
+    res.json({ mensaje: 'Iniciar sesión en Gretta' });
+});
+
+router.post('/registro', (req, res) => {
+    res.json({ mensaje: 'Registrarse en Gretta' });
+});
+
+export default router; //Cuando se exporta por default se puede importar con cualquier nombre
