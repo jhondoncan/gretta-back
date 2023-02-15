466b48e3035ba8a819d1a3599426001f3e70215c
Jhon Arlem Andrade <jhondoncan@outlook.com>
2023-02-15 17:34:20 -0500

    feat: :sparkles: Se configura servidor express con las rutas de Auth y ConexionBD

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import "./database/connectdb.js";
+import authRoutes from './routes/auth.route.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.json());
+
+
+app.use('/', authRoutes);
+
+app.get('/', (req, res) => {
+    res.json({
+        mensaje: "Bienvenido a Gretta",
+
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`✅ Servidor corriendo en: http://localhost:${PORT}`);
+});
